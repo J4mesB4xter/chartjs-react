1c253976ba35b6e82077f98ae42990fa3c61e678
unknown <jbj1996@gmail.com>
2021-08-06 16:06:04 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Polar from './PolarChart.js';
+import './index.css';
+import './App.css';
+
+function App() {
+  return (
+    <div class="chart-box">
+      <Polar />
+    </div>
+    
+  );
+}
+
+export default App;
